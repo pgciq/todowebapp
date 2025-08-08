46f9caff39519f617aad308dfb09229d67f777fe
pgciq <pgciq@126.com>
2025-08-08 10:29:44 +0800

    remove license declaration

--- src/main/java/io/github/faimoh/todowebapp/controllers/spring/RootController.java
+++ src/main/java/io/github/faimoh/todowebapp/controllers/spring/RootController.java
@@ -1,28 +1,3 @@
-/*
- * Copyright (c) 2020, Faisal Ahmed Pasha Mohammed https://github.com/faimoh
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 package io.github.faimoh.todowebapp.controllers.spring;
 
 import org.springframework.stereotype.Controller;
@@ -34,8 +9,6 @@ import jakarta.servlet.http.HttpSession;
 /**
  * Root Controller for Todo Web Application
  * Handles the main entry point and routing for the application
- * 
- * @author Faisal Ahmed Pasha Mohammed https://github.com/faimoh
  */
 @Controller
 public class RootController {
